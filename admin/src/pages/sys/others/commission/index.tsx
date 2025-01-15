8ac82fda0c0d27dd2322c66c29b98f0ed3fe7835
rolle_dev <89302745+toanhhg123@users.noreply.github.com>
2025-01-15 22:38:02 +0700

    chore: add total commission

--- admin/src/pages/sys/others/commission/index.tsx
+++ admin/src/pages/sys/others/commission/index.tsx
@@ -1,76 +1,78 @@
 import { useQuery } from "@tanstack/react-query";
 
 import {
-  type CommissionResponse,
-  default as apiCommission,
+	type CommissionResponse,
+	default as apiCommission,
 } from "@/api/services/commission";
-import type { OrderResponse } from "@/api/services/orderService";
 import ProTag from "@/theme/antd/components/tag";
 import { Card } from "antd";
 import Table, { type ColumnsType } from "antd/es/table";
 
 const FeedBackPage = () => {
-  // Lấy thông tin liên hệ của người dùng
-  const { data, isFetching } = useQuery({
-    queryKey: ["commission"],
-    queryFn: apiCommission.getAllCommission,
-  });
+	// Lấy thông tin liên hệ của người dùng
+	const { data, isFetching } = useQuery({
+		queryKey: ["commission"],
+		queryFn: apiCommission.getAllCommission,
+	});
 
-  const commissions = data || [];
+	const commissions = data || [];
 
-  console.log(commissions);
+	const commissionTotal = commissions.reduce((acc, cur) => acc + cur.price, 0);
 
-  const columns: ColumnsType<CommissionResponse> = [
-    {
-      title: "Id",
-      dataIndex: "id",
-      width: 60,
-    },
-    {
-      title: "Service Price",
-      dataIndex: ["order", "price"],
-      width: 60,
-      render: (price) => (
-        <ProTag color="geekblue">{price?.toLocaleString() || ""}</ProTag>
-      ),
-    },
-    {
-      title: "customer",
-      dataIndex: ["order", "customer"],
-      width: 60,
-      render: (customer) => (
-        <ProTag color="yellow-inverse">{customer?.fullName || ""}</ProTag>
-      ),
-    },
-    {
-      title: "employee",
-      dataIndex: "employee",
-      width: 60,
-      render: (_, record) => record?.order?.employee?.fullName || "",
-    },
-    {
-      title: "Price",
-      dataIndex: "price",
-      width: 100,
-      render: (price) => (
-        <ProTag color="gold">{price?.toLocaleString() || ""}</ProTag>
-      ),
-    },
-  ];
+	const columns: ColumnsType<CommissionResponse> = [
+		{
+			title: "Id",
+			dataIndex: "id",
+			width: 60,
+		},
+		{
+			title: "Service Price",
+			dataIndex: ["order", "price"],
+			width: 60,
+			render: (price) => (
+				<ProTag color="geekblue">{price?.toLocaleString() || ""}</ProTag>
+			),
+		},
+		{
+			title: "customer",
+			dataIndex: ["order", "customer"],
+			width: 60,
+			render: (customer) => (
+				<ProTag color="yellow-inverse">{customer?.fullName || ""}</ProTag>
+			),
+		},
+		{
+			title: "employee",
+			dataIndex: "employee",
+			width: 60,
+			render: (_, record) => record?.order?.employee?.fullName || "",
+		},
+		{
+			title: "Price",
+			dataIndex: "price",
+			width: 100,
+			render: (price) => (
+				<ProTag color="gold">{price?.toLocaleString() || ""}</ProTag>
+			),
+		},
+	];
 
-  return (
-    <Card title="Commission">
-      <Table
-        rowKey="id"
-        size="small"
-        scroll={{ x: "max-content" }}
-        pagination={false}
-        columns={columns}
-        dataSource={commissions}
-        loading={isFetching}
-      />
-    </Card>
-  );
+	return (
+		<Card
+			title="Commission"
+			extra={<ProTag color="gold">{commissionTotal.toLocaleString()}</ProTag>}
+		>
+			<Table
+				rowKey="id"
+				size="small"
+				scroll={{ x: "max-content" }}
+				pagination={false}
+				columns={columns}
+				dataSource={commissions}
+				loading={isFetching}
+			/>
+		</Card>
+	);
 };
 
 export default FeedBackPage;
